675926392afe2bcced7078e2ff24e440972b1627
IrenaGau <naudotojas@yahoo.com>
2022-04-11 15:07:53 +0300

    Galutonis

--- UogaUoga.ltAutoTests/Page/UogaCartPage.cs
+++ UogaUoga.ltAutoTests/Page/UogaCartPage.cs
@@ -16,7 +16,6 @@ namespace UogaUoga.ltAutoTests.Page
         private IWebElement IconPlusButton => Driver.FindElement(By.CssSelector("#cart_items > table > tbody > tr > td.amount.hidden-xs > form > div > div > span:nth-child(3) > button > span"));
         private IReadOnlyCollection<IWebElement> TotalSums => Driver.FindElements(By.XPath("//div[text()='Bendra suma:']//following::div[1]"));
 
-
         public void ClickOnIconPlusButton()
         {
             IconPlusButton.Click();
@@ -25,7 +24,6 @@ namespace UogaUoga.ltAutoTests.Page
         public void VerifyTotalSum()
         {
             Thread.Sleep(5000);
-            //GetWait().Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[text()='Bendra suma:']//following::div[1]")));
             Assert.IsTrue("47,85 €".Equals(TotalSums.ElementAt(1).Text), $"Text is not the same, actual text is {TotalSums.ElementAt(1).Text}");
         }
 
